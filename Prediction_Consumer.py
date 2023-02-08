945b1b459283337543553fb8def530a95e8511fa
syed-asif-mn <syedasifm.n@gmail.com>
2023-02-08 22:56:02 +0530

    Updated files

--- Prediction_Consumer.py
+++ Prediction_Consumer.py
@@ -6,6 +6,9 @@ import pickle
 import numpy as np
 from pyspark.ml.regression import RandomForestRegressionModel
 from pyspark.sql import DataFrame, SparkSession
+import pandas as pd
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.feature import VectorAssembler
 
 init_object = Initialize()
 spark = SparkSession.builder.appName("House Price Prediction").getOrCreate()
@@ -13,11 +16,10 @@ KAFKA_BROKER = os.getenv("KAFKA_BROKER")
 
 class Prediction_Consumer():
     
-    def predict_prize(self, features):
-        print(features)
-        predicted_cost = rf_model.predict(features)
-        return predicted_cost[0][0]
-
+    def predict_prize(self,features):
+        predicted_cost = rf_model.transform(features)
+        return predicted_cost
+    
     def gather_data(self):
         consumer = KafkaConsumer(auto_offset_reset='latest',
                                  bootstrap_servers=[KAFKA_BROKER], api_version=(0, 10),
@@ -28,15 +30,25 @@ class Prediction_Consumer():
         while True:
             for message in consumer:
                 if message.topic == "hackathon":
-                    value_bytes = (message.value.decode())
-                    print("Received Message: ")
-                    print(value_bytes)
-                    dense_vector_list = json.loads(value_bytes)
-                    dense_vector = np.array(dense_vector_list).reshape((-1, 1))
-                    predicted_cost = self.predict_prize(dense_vector)
-                    print ("Predicted Cost: ", predicted_cost)
+                    value = (message.value.decode())
+                    value = value.replace("[", "").replace("]", "")
+                    string_list = value.split(",")
+                    double_list = [float(x) for x in string_list]
+                    int_list = [int(x) for x in double_list]
+                    rdd = spark.sparkContext.parallelize([int_list])
+                    df = rdd.toDF(["values"])
+
+                    # Create an instance of the VectorAssembler
+                    assembler = VectorAssembler(inputCols=["values"], outputCol="independent_features")
+
+                    # Use the VectorAssembler to convert the column to the required type
+                    df = assembler.transform(df)
+                    
+                    predicted_cost = self.predict_prize(df)
+                    print ("Predicted Cost: ", predicted_cost.select("prediction"))
+                    
 
 if __name__ == '__main__':
+    rf_model = RandomForestRegressionModel.load("generated_models/rfmodel")
     pred_consumer = Prediction_Consumer()
     pred_consumer.gather_data()
-    rf_model = RandomForestRegressionModel.load("generated_models/rfmodel")
--- README.md
+++ README.md
@@ -9,25 +9,27 @@
 
   #### Dockerfile - Containerise the application
   #### House price prediction - training.ipynb - Model training and evaluation 
+      ##### Template code is provided in the "House price prediction - training.ipynb" notebook file to install requirements, data cleaning and training. 
   #### House price prediction - test.ipynb - Fitting the model on test dataset
-  #### Data - Folder with all the csv and streaming data files.
+      ##### Template code is provided in the "House price prediction - test.ipynb" notebook file to test the model created on test data. 
+  #### api.py - Publish the metrics and prediction output
+  #### Data - You will also be required to use the included train.csv - Training dataset and test.csv - Test dataset file.
   #### train.csv - Training dataset
   #### test.csv - Test dataset
-  #### synthesized_data - User Generated data
-  #### train_data_metrics.csv - Evaluation metrics of various machine learning models
-  #### final_predictions.csv - Final prediction of Sale Price values of the test data set 
+  #### Train_data_metrics.csv - Evaluation metrics of various machine learning models
+  #### final_submission.csv - Final prediction of Sale Price values of the test data set 
   #### linearmodel - Trained Linear Regression model 
   #### rfmodel - Trained Random Forest Regression model 
   #### gbrmodel - Trained Gradiant Boost Regression model 
   #### generated_models - folder contains all the model generated during training
   #### kafka_2.13-2.8.2 - Local Kafka Installation
-  #### config.yml - Used to configure kafka producer/consumer
+  #### config.yml - For configuration 
   #### Data_Producer.py - Kafka producer
   #### Dockerfile - To build docker container
-  #### docker-compose.yml - To configure and run multi-container Docker services
   #### Initializer.py - Class to initialize Kafka producer and consumer
   #### Prediction_Consumer.py - Kafka consumer in which consumer is loaded to predict the House Price
   #### requirements.txt - List of packages to be installed thorugh pip
   #### spark-3.3.1-bin-hadoop2.tgz - Local installation of spark
-
-  #### To Build and Run: docker-compose up --build
+  
+  #### To build an docker image: docker build -t hackathon .
+  #### To Run as container: docker run -p 9092:9092 -p 2181:2181 hackathon
